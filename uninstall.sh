081c42a29f9ab4d2deb1f2c827922a68f35b80f8
Hans-Filip Elo <fille@server02.hfelo.se>
2014-05-18 14:05:48 +0200

    Installer for newPrefs

--- uninstall.sh
+++ uninstall.sh
@@ -2,23 +2,28 @@
 # If something fails - exit
 set -e
 # Check if root
-if [[ whoami != 'root' ]]; then
+if [[ whoami -ne "root" ]]; then
 	echo "Must be root to run this installer."
 	exit 1;
 fi
 
 # Removes bashShot from crontab
 MYCRON=/tmp/mycron.txt
-(crontab -l | grep -iv bashshot | grep -iv "cleaner scripts") > $MYCRON
+(crontab -l | grep -iv bashshot) > $MYCRON
 crontab $MYCRON
 
 # Removes scripts
 rm /usr/bin/bashshot.sh
 rm /usr/bin/bashshot_cleaner.sh
+rm /usr/bin/bashshot_cleanerStarter.sh
+rm /usr/bin/array
 
 # Removes configuration
 rm -r /etc/bashshot
 
 # Removes file for log rotation
 rm /etc/logrotate.d/bashshot
+
+# Removes logfile
+rm /var/log/bashshot.log
 
